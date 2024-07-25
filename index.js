bac1d95b0cf0fc0bccfa07c1a12f25f1c4f2b7f3
pak <100640576+pakcyeong@users.noreply.github.com>
2024-07-25 11:29:23 +0900

    .

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ dotenv.config();
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
-client.cooldowns = new Collection();
 
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
