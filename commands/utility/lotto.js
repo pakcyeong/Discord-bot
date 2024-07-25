85b4333a1e86c919bcf271c0ebc81d30afcfbfb9
pak <100640576+pakcyeong@users.noreply.github.com>
2024-07-25 10:41:14 +0900

    .

--- commands/utility/lotto.js
+++ commands/utility/lotto.js
@@ -1,11 +1,23 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const fs = require("fs");
+const path = '../../components/json/lotto.json';
 
 module.exports = {
+    cooldown: 1800,
     data: new SlashCommandBuilder()
         .setName("강화")
         .setDescription("로스트아크 강화 확률로 보는 오늘의 운세 입니다."),
 
     async execute(interaction){
+        const graph = [100, 100, 50, 50, 40, 40, 30, 30, 15, 15, 10, 10, 5, 5];
+        const usr = interaction.user.id;
+        const j = fs.readFileSync(path, {encoding: 'utf-8', flag: 'r'});
+        const data = JSON.parse(j);
+        let key = [];
+        
+        if(Object.hasOwn(data, usr)) {key = data[usr];}
+        else {key = [10,0]}
+
         const prob = Math.floor(Math.random()*1000);
         const li = [
             '당연히 실패했다 음머, 다음에 도전하도록',
@@ -23,10 +35,29 @@ module.exports = {
         ];
         const fail = Math.floor(Math.random()*li.length);
         
-        if(prob < 15){
-            await interaction.reply({content: '강화에 성공했다. 스택 초기화다 음머'})
-        }else{
-            await interaction.reply({content: `${li[fail]}!`});
+        if(key[0]*1>26) {
+            if(prob < graph[(key[0]*1)-10]){
+                key[1] = 0;
+                data[usr] = key;
+                fs.writeFileSync(path,JSON.stringify(data));
+                await interaction.reply({content: `강화에 성공했다. 음머 \`현재 재련 단계 : ${key[0]*1}\` (장기 ${key[1]*1}%)`})
+            }else{
+                if(key[1]==100){
+                    key[1] = 0;
+                    data[usr] = key;
+                    fs.writeFileSync(path,JSON.stringify(data));
+                    await interaction.reply({content: `원트 ㅊㅊ, 스택 초기화다. 음머 \`현재 재련 단계 : ${key[0]*1}\` (장기 ${key[1]*1}%)`})
+                }
+                else{
+                    (key[1]*1)+=(graph[(key[0]*1)-10]/25);
+                    data[usr] = key;
+                    fs.writeFileSync(path,JSON.stringify(data));
+                    await interaction.reply({content: `${li[fail]}! \`현재 재련 단계 : ${key[0]}\` (장기 ${key[1]*1}%)`});
+                }
+            }
+        }
+        else{
+            await interaction.reply({content: `이미 최고 강화 단계에 도달 하셨습니다. 음머`})
         }
     }
 }
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -3,37 +3,46 @@ const { Events } = require('discord.js');
 module.exports = {
 	name: Events.InteractionCreate,
 	async execute(interaction) {
-		if(interaction.isChatInputCommand()){
-			const command = interaction.client.commands.get(interaction.commandName);
+		if(!interaction.isChatInputCommand()) return;
 
-			if (!command) {
-				console.error(`No command matching ${interaction.commandName} was found.`);
-				return;
-			}
+		const command = interaction.client.commands.get(interaction.commandName);
 
-			try {
-				await command.execute(interaction);
-			} catch (error) {
-				console.error(error);
-				if (interaction.replied || interaction.deferred) {
-					await interaction.followUp({ content: '명령어 실행에서 문제가 발생했다 음머!', ephemeral: true });
-				} else {
-					await interaction.reply({ content: '명령어 실행에서 문제가 발생했다 음머!', ephemeral: true });
-				}
+		const { cooldowns } = interaction.client;
+
+		if (!cooldowns.has(command.data.name)) {
+			cooldowns.set(command.data.name, new Collection());
+		}
+
+		const now = Date.now();
+		const timestamps = cooldowns.get(command.data.name);
+		const defaultCooldownDuration = 3;
+		const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1_000;
+
+		if (timestamps.has(interaction.user.id)) {
+			const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+
+			if (now < expirationTime) {
+				const expiredTimestamp = Math.round(expirationTime / 1_000);
+				return interaction.reply({ content: `잠시 기다려달라. 음머 \`${command.data.name}\`. 시간뒤에 다시 할 수 있다. <t:${expiredTimestamp}:R>.`, ephemeral: true });
 			}
 		}
-		else if (interaction.isAutocomplete()){
-			const command = interaction.client.commands.get(interaction.commandName);
 
-			if (!command) {
-				console.error(`No command matching ${interaction.commandName} was found.`);
-				return;
-			}
+		timestamps.set(interaction.user.id, now);
+		setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
 
-			try {
-				await command.autocomplete(interaction);
-			} catch (error) {
-				console.error(error);
+		if (!command) {
+			console.error(`No command matching ${interaction.commandName} was found.`);
+			return;
+		}
+
+		try {
+			await command.execute(interaction);
+		} catch (error) {
+			console.error(error);
+			if (interaction.replied || interaction.deferred) {
+				await interaction.followUp({ content: '명령어 실행에서 문제가 발생했다 음머!', ephemeral: true });
+			} else {
+				await interaction.reply({ content: '명령어 실행에서 문제가 발생했다 음머!', ephemeral: true });
 			}
 		}
 	},
