a182fb33a6e18ed97114cf73a18c9ea90b09db0a
pak <100640576+pakcyeong@users.noreply.github.com>
2024-08-29 17:41:25 +0900

    test

--- components/regexp.js
+++ components/regexp.js
@@ -2,12 +2,12 @@ function regexp(string){
     let result = string;
 
     const regexpHTML = /(<([^>]+)>[\s]*)|\\r\\n|\\|/gi;
-    const regexpSTRStart = /"{/g;
-    const regexpSTREnd = /}"/g;
+    // const regexpSTRStart = /"{/g;
+    // const regexpSTREnd = /}"/g;
 
     result = result.replace(regexpHTML, '');
-    result = result.replace(regexpSTRStart, '{');
-    result = result.replace(regexpSTREnd, '}'); 
+    // result = result.replace(regexpSTRStart, '{');
+    // result = result.replace(regexpSTREnd, '}'); 
 
     return result;
 }
