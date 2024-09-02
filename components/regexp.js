9caab554a6ba71371c11dcf3a60a59575e056d14
pak <100640576+pakcyeong@users.noreply.github.com>
2024-09-02 11:04:34 +0900

    0902

--- components/regexp.js
+++ components/regexp.js
@@ -1,7 +1,7 @@
 function regexp(string){
     let result = string;
 
-    const regexpHTML = /(<([^>]+)>[\s]*)|\\r\\n|\\|/gi;
+    const regexpHTML = /(<([^>]+)>[\s]*)|\\r\\n|\\|도약|깨달음|진화/gi;
 
     result = result.replace(regexpHTML, '');
 
