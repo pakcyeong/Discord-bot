8d134727fe914061b704d81e02b3a3b2ea7d207b
pak <100640576+pakcyeong@users.noreply.github.com>
2024-09-02 11:58:02 +0900

    0902

--- components/regexp.js
+++ components/regexp.js
@@ -1,11 +1,13 @@
 function regexp(string){
     let result = string;
-
-    const regexpHTML = /(<([^>]+)>[\s]*)|\\r\\n|\\|도약|깨달음|진화/gi;
-
+    const regexpHTML = /(<([^>]+)>[\s]*)|\\r\\n|\\|진화|깨달음|도약/gi;
     result = result.replace(regexpHTML, '');
 
-    return result;
+    const tier = result.split(' ');
+
+    return `\`T${tier[0]}\` ${result.replace(tier[0], '')}`;
 }
 
 module.exports = regexp;
+
+// \`T${n}\` ${Title} Lv. ${n} => expect value
