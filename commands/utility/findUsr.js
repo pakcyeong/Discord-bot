1ef9e904c72b05441ab556528fa56f8c73a7724a
pak <100640576+pakcyeong@users.noreply.github.com>
2024-09-02 10:38:26 +0900

    test

---
+++ commands/utility/findUsr.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder } = require("discord.js");
+const research = require("../../components/research");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('조회')
+        .setDescription()
+        .addStringOption(option => 
+            option
+                .setName('유저명')
+                .setDescription('유저 닉네임')
+                .setRequired(true)
+        ),
+    async execute(interaction){
+        const usrName = interaction.options.getString('유저명');
+        const usrData = await research(usrName);
+
+        await interaction.reply({ content: usrName, ephemeral: true })
+    }
+}
