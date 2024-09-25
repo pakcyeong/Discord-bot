e61879b48ebf115c4542fcba3245f30cae304056
pak <100640576+pakcyeong@users.noreply.github.com>
2024-09-25 11:16:52 +0900

    Update cubeExCalc.js

--- components/cubeExCalc.js
+++ components/cubeExCalc.js
@@ -1,7 +1,7 @@
-function cubeExCalc(tic1){
+function cubeExCalc(tic1, tic2){
 
     // ticket level
-    const tickets = [tic1*1];
+    const tickets = [tic1*1, tic2*1];
 
     // Gem calc: gem(lv.i)*3 = gem(lv.i+1)
     const gems = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
@@ -10,6 +10,7 @@ function cubeExCalc(tic1){
     // Cube avg rewards: gems, itm1, card, itm2, itm3, money 
     const rewards = [
         [9, 14, 14000, 4, 4, 140100],
+        [18, 25, 14500, 5, 5, 146800]
     ];
 
     // tickets => expect rewards 
